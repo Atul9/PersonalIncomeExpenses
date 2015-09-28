7e7ca8c365f67195a9901e71324d98632b1e8f48
Atul Bhosale <atul1bhosale@gmail.com>
2015-09-28 12:18:42 +0530

    Modified query to display user related trasctions

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,7 @@
 class ReportsController < ApplicationController
   def expenses
     if request.post?
-      @report = Expense.includes(:expense_category).where(expense_category_id: params[:search][:expenses_category_id], record_date: params[:search][:start_date].to_date..params[:search][:end_date].to_date)
+      @report = Expense.includes(:expense_category).where(expense_category_id: params[:search][:expenses_category_id], record_date: params[:search][:start_date].to_date..params[:search][:end_date].to_date, user: current_user)
     end
   end
 end
--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,7 +3,7 @@ class TransactionsController < ApplicationController
   def index
     @transactions = Transaction.includes(:recordable).where(user_id: current_user.id)
     if params[:search].present? && !(params[:search][:start_date].blank? || params[:search][:end_date].blank?)
-      @transactions = Transaction.includes(:recordable).where(transaction_date: params[:search][:start_date].to_date..params[:search][:end_date].to_date)
+      @transactions = Transaction.includes(:recordable).where(transaction_date: params[:search][:start_date].to_date..params[:search][:end_date].to_date, user_id: current_user.id)
     end
   end
 end
