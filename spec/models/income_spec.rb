b424ea0e894d20b08f91b16c27b044158c9c8670
Atul Bhosale <atul1bhosale@gmail.com>
2015-10-23 23:47:58 +0530

    Added income specs

--- spec/models/income_spec.rb
+++ spec/models/income_spec.rb
@@ -2,10 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Income, type: :model do
   describe 'validates income category'do
-    it 'should create income with an income category' do
+    it 'should not create income without an income category' do
       income = FactoryGirl.build :income
       expect(income.save).to eq(false)
       expect(income.errors.messages).to eq({:income_category_id=>["can't be blank"], :user_id=>["can't be blank"]})
+    end
+  end
+
+  describe 'validated income amount' do
+    it 'validates amount' do
+      user = FactoryGirl.create :user
+      income = FactoryGirl.build(:income, income_category_id: IncomeCategory.first.id, user: user)
+      income.amount = "A"
+      expect(income.save).to eq(false)
+      p income.errors.messages
     end
   end
 end
